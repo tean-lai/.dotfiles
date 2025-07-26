ddf10d4c8f6b1a284fdada6f363391252edbccf5
Tean Lai <laitean19@gmail.com>
2025-07-25 23:30:54 -0700

    backup neovim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,4 +1,4 @@
-vim.cmd('source ~/.vimrc')
+-- vim.cmd('source ~/.vimrc')
 
 
 -- vim.cmd("colorsheme tokyonight")
@@ -12,17 +12,24 @@ vim.cmd('source ~/.vimrc')
 --
 -- vim.api.nvim_set_keymap("n", "<leader>s", ":silent !$SHELL<CR>", { noremap = true, silent = true })
 --
--- local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
--- if not (vim.uv or vim.loop).fs_stat(lazypath) then
---   vim.fn.system({
---     "git",
---     "clone",
---     "--filter=blob:none",
---     "https://github.com/folke/lazy.nvim.git",
---     "--branch=stable", -- latest stable release
---     lazypath,
---   })
--- end
--- vim.opt.rtp:prepend(lazypath)
---
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+	"nvim-lualine/lualine.nvim",
+	"nvim-treesitter/nvim-treesitter",
+	-- "nvim-telescope/telescope.nvim",
+	"neovim/nvim-lspconfig"
+})
+
 -- require("lazy").setup("plugins")
