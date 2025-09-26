134ac4c80615c17f8a9f3dcfd2e5c120a95c8e6f
Tean Lai <laitean19@gmail.com>
2025-09-26 12:44:58 -0400

    add basic nvim config

---
+++ .config/nvim/lua/remap.lua
@@ -0,0 +1,15 @@
+
+-- For first two remaps, when in visual mode, pressing J and K will move the line of code up and down
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Apparently, this will let you type a replacement, and replace every word under your cursor with that
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlights text when yanking",
+  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
